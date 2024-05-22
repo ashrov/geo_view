7e8448ab5d981fd74a3d94ab7c3ff0ae537f6386
Anton Sharov <a.sharov@adriver.ru>
2024-05-22 21:44:07 +0300

    Add location getting

--- geo_view/views.py
+++ geo_view/views.py
@@ -1,25 +1,38 @@
-from datetime import datetime
-from typing import Literal, Any
+from datetime import date
+from typing import Any
 
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
-from pydantic import BaseModel, ValidationError, Field, field_validator
+from pydantic import BaseModel, ValidationError, field_validator
+
+from geo_view.models import GeoPosition
 
 
 class SearchOptions(BaseModel):
-    start: datetime | None = None
-    end: datetime | None = None
+    start: date | None = None
+    end: date | None = None
+    limit: int | None = None
 
     @field_validator('start', 'end', mode='before')
     @classmethod
-    def validate_datetime(cls, value: Any) -> datetime | None:
+    def validate_datetime(cls, value: Any) -> date | None:
         if not value:
             return None
-        return datetime.fromisoformat(value)
+        return date.fromisoformat(value)
 
     @property
     def as_query(self) -> str:
         return '&'.join(f'{field}={value}' for field, value in self.model_dump(mode='json', exclude_defaults=True).items())
+
+    def get_positions(self) -> list[GeoPosition]:
+        postions = GeoPosition.objects.all()
+        if self.start:
+            postions = postions.filter(article__date__gte=self.start)
+        if self.end:
+            postions = postions.filter(article__date__lt=self.start)
+        if self.limit:
+            postions = postions[:self.limit]
+        return list(postions)
 
 
 def search_view(request: HttpRequest) -> HttpResponse:
@@ -50,12 +63,12 @@ def search_view(request: HttpRequest) -> HttpResponse:
 
 def map_view(request: HttpRequest) -> HttpResponse:
     """View для отображения карты."""
+    options = SearchOptions(**request.GET.dict())
+
     return render(
         request,
         'map.html',
         context={
-            'positions': [
-                (10.1, 20.1),
-            ],
+            'positions': options.get_positions(),
         },
     )
